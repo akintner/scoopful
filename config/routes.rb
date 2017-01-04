7359490785fdd9a7f1738c4722294215d706fed2
Mike Schutte <tmikeschutte@gmail.com>
2017-01-04 15:45:33 -0700

    Add cart and carts path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   resources 'items', only: [:index]
+  
+  resources :carts, only: [:create]
+  get '/cart', to: 'carts#index'
 
   get '/:category_name', to: "categories#show"
+
 
 end

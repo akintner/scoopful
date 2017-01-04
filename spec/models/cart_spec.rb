31697b9141fd7b54b050207039b2a11d9ed79163
Mike Schutte <tmikeschutte@gmail.com>
2017-01-04 16:20:08 -0700

    Update cart model test
    All methods tested. Room for edge cases.

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,12 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+  before do
+    @cart = Cart.new({})
+    @items = create_list(:item, 3)
+  end
+
   it "adds an item to its contents" do
-    backpack = Backpack.new
-    expect(backpack.total_count).to eq 0
+    expect(@cart.contents.empty?).to be_truthy
 
-    backpack.add_item(2)
+    @cart.add_item(1)
 
-    expect(backpack.total_count).to eq 1
+    expect(@cart.contents.empty?).to be_falsy
+    expect(@cart.contents.count).to eq 1
+  end
+
+  it "adds many of one item if quantity is specified" do
+    expect(@cart.contents.empty?).to be_truthy
+
+    @cart.add_item(1, 3)
+
+    expect(@cart.contents.empty?).to be_falsy
+    expect(@cart.contents.values.first).to eq 3
+  end
+
+  it "counts all the items it has" do
+    expect(@cart.total_count).to eq 0
+
+    @cart.add_item(2)
+    @cart.add_item(5)
+    @cart.add_item(5)
+    @cart.add_item(3)
+
+    expect(@cart.total_count).to eq 4
+  end
+
+  it "counts the amount of an item" do
+    @cart.add_item(1)
+    @cart.add_item(1)
+    expect(@cart.count_of(1)).to eq 2
+  end
+
+  it "returns an active record collection of all items" do
+    @items.each { |item| @cart.add_item(item.id) }
+    
+    expect(@cart.items.class.to_s).to eq "Item::ActiveRecord_Relation"
+    expect(@cart.items.count).to eq 3
+  end
+
+  it "calculates the total price of all items" do
+    @items.each { |item| @cart.add_item(item.id) }
+
+    total = @items.map(&:price_per_unit).sum.round(2)
+    expect(@cart.total_price).to eq total
   end
 end

4daea95b0919394aba473c1c76396e05265e052e
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-03 18:06:01 -0700

    Feature test for visitor views item passing.

--- spec/features/items/visitor_views_items_spec.rb
+++ spec/features/items/visitor_views_items_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 RSpec.describe do
   context "visitor" do
     scenario "sees all items" do
+      items = create_list(:item, 2)
 
       visit items_path
 
-
-
+      items.each do |item|
+        expect(page).to have_content item.name
+        expect(page).to have_content item.description
+        expect(page).to have_content item.price_per_unit
+        expect(page).to have_css "img[src=\"#{item.image_url}\"]"
+      end
     end
   end
 end

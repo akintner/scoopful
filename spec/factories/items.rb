5af38b3f310f178b5dbc92642f0bd200f8c76a26
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-04 13:20:22 -0700

    Change item factory to ensure unique name.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :item do
-    name { Faker::Food.ingredient }
+    sequence :name do |n|
+      Faker::Food.ingredient + "-#{n}"
+    end
     description { Faker::Hipster.sentence }
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'images', 'grapes.png')) } 
     price_per_unit { Faker::Number.decimal(1, 2) }

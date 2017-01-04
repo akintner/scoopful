eef3a6eddb63888c6ba617fe8062339c68e4aede
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-04 15:11:25 -0700

    Shorten item descriptions in seed files.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,39 +3,28 @@ nuts = Category.create(title: 'Nuts')
 
 nuts.items.create(
   name: 'Almonds',
-  description: 'The almond that we think of as a nut is technically the seed of the fruit of the almond tree,
-  a medium-size tree that bears fragrant pink and white flowers. Like its cousins, the peach, cherry 
-  and apricot trees, the almond tree bears fruits with stone-like seeds (or pits) within.',
+  description: "Humankind's most beloved nut.",
   price_per_unit: 5.38,
   image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'db', 'images', 'almonds.png'))
 )
 
 nuts.items.create(
   name: 'Walnuts',
-  description: 'A walnut is the nut of any tree of the genus Juglans (Family Juglandaceae),
-  particularly the Persian or English walnut, Juglans regia. Technically a walnut is the
-  seed of a drupe or drupaceous nut, and thus not a true botanical nut.
-  The walnut is nutrient-dense with protein and essential fatty acids.',
+  description: 'Nutrient-dense with protein and essential fatty acids.',
   price_per_unit: 4.21,
   image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'db', 'images', 'walnuts.png'))
 )
 
 coffees.items.create(
   name: 'Colombian',
-  description: 'Colombian coffee is often regarded as some of the highest quality coffee in the world.
-  Colombia has traditionally grown arabica beans and its unique geography makes it perfectly suited
-  for producing a high quality brew. Coffee production in Colombia has a reputation as producing mild,
-  balanced beans.',
+  description: 'Mild, balanced beans.',
   price_per_unit: 1.72,
   image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'db', 'images', 'colombian.png'))
 )
 
 coffees.items.create(
   name: 'Caturra',
-  description: 'Caturra is distinguished by its bright acidity and low-to-medium body.
-  It has less clarity and sweetness than its parent, Bourbon. Also known as Variedad 
-  Colombia, you’ll taste the classic caramel and chocolate with hints of cherry 
-  in the sweet, bright, full-bodied bean.',
+  description: 'Bright acidity and low-to-medium body.',
   price_per_unit: 6.27,
   image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'db', 'images', 'caturra.png'))
 )

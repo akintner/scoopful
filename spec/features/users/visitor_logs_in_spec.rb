2b8f62b86832bd2280e7275c92337f67bbc9b2ac
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-05 14:07:38 -0700

    Log in feature test.

---
+++ spec/features/users/visitor_logs_in_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature do
+  context 'visitor' do
+    scenario 'sees login page' do 
+      visit root_path
+
+      expect(page).to have_link 'Login'
+
+      click_on 'Login'
+
+      expect(current_path).to eq login_path
+      expect(page).to have_field :email
+      expect(page).to have_field :password
+      expect(page).to have_field :password_confirmation
+      expect(page).to have_link 'Create Account'
+    end
+
+    scenario 'logs in' do
+      visit login_path
+
+      click_on 'Create Account'
+
+      fill_in :name, with: 'Arya'
+      fill_in :email, with: 'agirl@hasnoname.com'
+      fill_in :password, with: 'password'
+      fill_in :password_confirmation, with: 'password'
+      click_on 'Submit'
+
+      expect(current_path).to eq dashboard_path
+      expect(page).to have_content 'Logged in as Arya'
+      expect(page).to have_content 'Arya'
+      expect(page).to have_content 'agirl@hasnoname.com'
+      expect(page).to_not have_link 'Login'
+      expect(page).to have_link 'Logout'
+    end
+  end
+end

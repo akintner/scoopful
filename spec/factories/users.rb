6a386ecff57692e29c09d5a3a3a1b2ece1b6a19f
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-05 16:48:49 -0700

    Trailing whitespace deleted.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     password 'password'
-    sequence :email do |n| 
+    sequence :email do |n|
       Faker::Internet.email + "-#{n}"
     end
   end

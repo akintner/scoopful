903ff0194d2837434d703c3a194258a186b9e32e
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-04 20:49:16 -0700

    Individual tags for items

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -19,7 +19,9 @@ class CartsController < ApplicationController
     item = Item.find(params[:item_id])
     @cart.remove(item.id)
 
-    flash[:success] = "Successfully removed #{link_to item.name, items_path} from your cart."
+    flash[:success] = "Successfully removed
+    #{view_context.link_to(item.name, items_path(anchor: item.name))}
+    from your cart."
     redirect_to cart_path
   end
 end

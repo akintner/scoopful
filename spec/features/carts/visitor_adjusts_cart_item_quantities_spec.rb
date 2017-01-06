6a5224974ff4a5391c87b8a1138ae30237a95374
Mike Schutte <tmikeschutte@gmail.com>
2017-01-05 17:45:52 -0700

    Remove user story comment

--- spec/features/carts/visitor_adjusts_cart_item_quantities_spec.rb
+++ spec/features/carts/visitor_adjusts_cart_item_quantities_spec.rb
@@ -1,19 +1,3 @@
-=begin 
-As a visitor
-When I visit "/cart"
-Then I should see my item with a quantity of 1
-And when I increase the quantity
-Then my current page should be '/cart'
-And that item's quantity should reflect the increase
-And the subtotal for that item should increase
-And the total for the cart should match that increase
-And when I decrease the quantity
-Then my current page should be '/cart'
-And that item's quantity should reflect the decrease
-And the subtotal for that item should decrease
-And the total for the cart should match that decrease
-=end
-
 require 'rails_helper'
 
 RSpec.feature do

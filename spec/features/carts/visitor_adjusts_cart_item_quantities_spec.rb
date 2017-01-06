d53c8fbf2a66b5a5163540573b41c58233731b6e
Mike Schutte <tmikeschutte@gmail.com>
2017-01-05 17:44:58 -0700

    Add passing tests for adjust cart items

--- spec/features/carts/visitor_adjusts_cart_item_quantities_spec.rb
+++ spec/features/carts/visitor_adjusts_cart_item_quantities_spec.rb
@@ -51,19 +51,76 @@ RSpec.feature do
         expect(page).to have_content @item.name
         expect(find_field('Quantity').value).to eq '2'
       end
-      expect(page).to have_content "Subtotal: $#{@item.price_per_unit * 2}"
+
       new_total = (@item.price_per_unit * 2 + @item2.price_per_unit).round(2)
+      expect(page).to have_content "Subtotal: $#{@item.price_per_unit * 2}"
       expect(page).to have_content "Total Price: $#{new_total}"
       expect(old_total).to_not eq new_total
     end
 
-    xscenario 'increases an item\'s quantity by three' do
+    scenario 'increases an item\'s quantity by three' do
+      visit cart_path
+      old_total = (@item.price_per_unit + @item2.price_per_unit).round(2)
+
+      within 'li.cart-item:nth-child(1)' do
+        fill_in 'quantity', with: '4'
+        click_on 'Update Quantity'
+
+        expect(current_path).to eq cart_path
+        expect(page).to have_content @item.name
+        expect(find_field('Quantity').value).to eq '4'
+      end
+
+      new_total = (@item.price_per_unit * 4 + @item2.price_per_unit).round(2)
+      expect(page).to have_content "Subtotal: $#{@item.price_per_unit * 4}"
+      expect(page).to have_content "Total Price: $#{new_total}"
+      expect(old_total).to_not eq new_total
     end
 
-    xscenario 'decreases an item\'s quantity by one' do
+    scenario 'decreases an item\'s quantity by one' do
+      visit cart_path
+      old_total = (@item.price_per_unit + @item2.price_per_unit).round(2)
+
+      within 'li.cart-item:nth-child(1)' do
+        fill_in 'quantity', with: '0'
+        click_on 'Update Quantity'
+
+        expect(current_path).to eq cart_path
+        expect(page).to have_content @item.name
+        expect(find_field('Quantity').value).to eq '0'
+      end
+
+      new_total = (@item.price_per_unit * 0 + @item2.price_per_unit).round(2)
+      expect(page).to have_content "Subtotal: $#{@item.price_per_unit * 0}"
+      expect(page).to have_content "Total Price: $#{new_total}"
+      expect(old_total).to_not eq new_total
     end
 
-    xscenario 'decreases an item\'s quantity by two' do
+    scenario 'decreases an item\'s quantity by two' do
+      visit items_path
+      within "section##{@item.name.tr(' ', '-')}" do
+        click_button('Add to Cart')
+        click_button('Add to Cart')
+        click_button('Add to Cart')
+      end
+
+      visit cart_path
+      old_total = (@item.price_per_unit * 4 + @item2.price_per_unit).round(2)
+
+      within 'li.cart-item:nth-child(1)' do
+        expect(find_field('Quantity').value).to eq '4'
+        fill_in 'quantity', with: '1'
+        click_on 'Update Quantity'
+
+        expect(current_path).to eq cart_path
+        expect(page).to have_content @item.name
+        expect(find_field('Quantity').value).to eq '1'
+      end
+
+      new_total = (@item.price_per_unit * 1 + @item2.price_per_unit).round(2)
+      expect(page).to have_content "Subtotal: $#{@item.price_per_unit * 1}"
+      expect(page).to have_content "Total Price: $#{new_total}"
+      expect(old_total).to_not eq new_total
     end
   end
 end

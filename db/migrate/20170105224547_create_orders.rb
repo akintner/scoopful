675b14c3aab6144aaef27183c434925da872f608
Amz K <akintner@u.rochester.edu>
2017-01-05 17:35:15 -0700

    update foreign key on orders migration

--- db/migrate/20170105224547_create_orders.rb
+++ db/migrate/20170105224547_create_orders.rb
@@ -2,8 +2,9 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
       t.float :total_cost
+      t.references :user, foreign_key: true
 
-      t.timestamps, null: false
+      t.timestamps
     end
   end
 end

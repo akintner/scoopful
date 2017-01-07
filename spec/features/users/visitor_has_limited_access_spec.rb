cd0f3978e85d6ed366f3929bdd7b38b0f384e29f
Mike Schutte <tmikeschutte@gmail.com>
2017-01-06 17:34:55 -0700

    Add passing spec for visitor access

--- spec/features/users/visitor_has_limited_access_spec.rb
+++ spec/features/users/visitor_has_limited_access_spec.rb
@@ -1,30 +1,30 @@
-=begin 
-Background: An unauthenticated user and their abilities
-As an Unuthenticated User
-I cannot view another user's private data, such as current order, etc.
-I should be redirected to login/create account when I try to check out.
-I cannot view the administrator screens or use administrator functionality.
-I cannot make myself an administrator.
-=end
-
 require 'rails_helper'
 
 RSpec.feature do
-  before do
-    @user = create(:user_with_orders)
-  end
-
   context 'visitor' do
-    scenario 'cannot view another user\'s orders' do
+    scenario 'cannot view /orders' do
       visit orders_path
 
-      expect_404
+      expect_login_path
     end
 
-    scenario 'cannot view administrator dashboard' do
+    xscenario 'cannot view /orders/:id' do #waiting on order show functionality
+      order = create(:order)
+      visit orders_path(order)
+
+      expect_login_path
+    end
+
+    scenario 'cannot view /dashboard' do
+      visit dashboard_path
+
+      expect_login_path
+    end
+
+    scenario 'cannot view /admin/dashboard' do
       visit admin_dashboard_path
 
-      expect_404
+      expect_login_path
     end
   end
 end

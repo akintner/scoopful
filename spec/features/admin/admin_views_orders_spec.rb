13d279365a8e43e1fb4b0b80dd4d377c5bbcc07a
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-07 14:57:04 -0700

    Refactor feature test for filter by orders.

--- spec/features/admin/admin_views_orders_spec.rb
+++ spec/features/admin/admin_views_orders_spec.rb
@@ -23,30 +23,13 @@ RSpec.feature do
       end
 
       scenario 'filter orders by status' do
-        click_on 'ordered'
-
-        expect(current_path).to eq admin_dashboard_path
-        expect(page).to have_content 'Ordered Orders'
-        expect_only_orders('ordered?')
-
-        click_on 'paid'
-
-        expect(current_path).to eq admin_dashboard_path
-        expect(page).to have_content 'Paid Orders'
-        expect_only_orders('paid?')
-
-        click_on 'cancelled'
-
-        expect(current_path).to eq admin_dashboard_path
-        expect(page).to have_content 'Cancelled Orders'
-        expect_only_orders('cancelled?')
-
-        click_on 'completed'
-
-        expect(current_path).to eq admin_dashboard_path
-        expect(page).to have_content 'Completed Orders'
-        expect_only_orders('completed?')
-
+        Order.statuses.keys.each do |status|
+          click_on status
+          expect(current_path).to eq admin_dashboard_path
+          expect(page).to have_content "#{status.capitalize} Orders"
+          expect_only_orders("#{status}?")
+        end
+      
         click_on 'all'
 
         expect(current_path).to eq admin_dashboard_path
@@ -55,24 +38,6 @@ RSpec.feature do
           expect(page).to have_content order.id
         end
       end
-
-      scenario ' transitions between statuses' do
-
-      end
     end
   end
 end
-
-# issueEighteen:
-#    title: Admin Orders
-#    body: >
-#      As an Admin
-#      When I visit the dashboard
-#      Then I can see a listing of all orders
-#      And I can see the total number of orders for each status **("Ordered", "Paid", "Cancelled", "Completed")**
-#      And I can see a link for each individual order
-#      And I can filter orders to display by each status type  **("Ordered", "Paid", "Cancelled", "Completed")**
-#      And I have links to transition between statuses
-#        - I can click on "cancel" on individual orders which are "paid" or "ordered"
-#        - I can click on "mark as paid" on orders that are "ordered"
-#        - I can click on "mark as completed" on orders that are "paid"

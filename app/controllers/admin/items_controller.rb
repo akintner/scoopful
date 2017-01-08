02da99910d8e2e463ea18c0425df3dbad6b388be
Mike Schutte <tmikeschutte@gmail.com>
2017-01-08 14:47:05 -0700

    Add before actions

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,21 @@
 class Admin::ItemsController < Admin::BaseController
+  before_action :set_item, only: [:edit, :update]
+  before_action :set_statuses, only: [:edit, :update]
+
   def index
     @items = Item.all
   end
 
   def edit
-    @statuses = Item.statuses
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       flash[:success] = 'Item Updated!'
       redirect_to admin_items_path
     else
+      flash.now[:danger] = @item.errors.full_messages.join('.')
+      render :edit
     end
   end
 
@@ -21,5 +23,13 @@ class Admin::ItemsController < Admin::BaseController
 
   def item_params
     params.require(:item).permit(:name, :description, :status, :image)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_statuses
+    @statuses = Item.statuses
   end
 end

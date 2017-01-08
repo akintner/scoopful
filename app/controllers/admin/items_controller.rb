ee2ae7c8e6b72952b3835dd73d970bd7bd242bc4
Mike Schutte <tmikeschutte@gmail.com>
2017-01-08 12:49:22 -0700

    Develop update action for admin items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,5 +8,17 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      flash[:success] = 'Item Updated!'
+      redirect_to admin_items_path
+    else
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
   end
 end

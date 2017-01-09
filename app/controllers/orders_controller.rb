8953a5b00d25c28bbf2c44008bb5042a6564d99f
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-09 11:53:43 -0700

    Edit update order flash message.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,7 +21,7 @@ class OrdersController < ApplicationController
   def update
     order = Order.find(params[:id])
     order.update(status: params[:status].to_i)
-    flash[:success] = "Order #{order.id} successfully updated"
+    flash[:success] = "Order #{order.id} updated to #{order.status}"
     redirect_to admin_dashboard_path
   end
 end

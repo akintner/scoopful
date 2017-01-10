82caaf48a754cb7e41529ba0a375c4f851c795ff
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2017-01-10 11:35:08 -0700

    Delete trailing whitespace.

--- app/models/item_analytics.rb
+++ app/models/item_analytics.rb
@@ -2,7 +2,7 @@ module ItemAnalytics
 
   def self.most_popular
     item_quantities = Item.joins(:orders_items).group(:item_id).sum(:quantity)
-    max_quantity = item_quantities.values.max 
+    max_quantity = item_quantities.values.max
     Item.find(item_quantities.key(max_quantity))
   end
 
